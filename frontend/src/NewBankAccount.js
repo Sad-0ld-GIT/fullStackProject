c0a6371a614dc869cc7d5c4d1326280f32bf6527
user <user@email.com>
2023-07-28 17:58:34 +0100

    react project modified inputs

--- frontend/src/NewBankAccount.js
+++ frontend/src/NewBankAccount.js
@@ -7,6 +7,7 @@ function NewBankAccount(){
     function processData(data){
         //alert(data.accountNumber)
         setaccountNumber(data)
+        // alert(`Account created: ${data.accountNumber}`)
     }
 
     function processResponse(response){
@@ -21,7 +22,8 @@ function NewBankAccount(){
             body: JSON.stringify({
                 "name":document.getElementById("na").value,
                 "country":document.getElementById("country").value,
-                "accountType":document.getElementById("acctype").value
+                // "accountType":document.getElementsByName("acctype").value,
+                "accountType": document.querySelector('input[name="acctype"]:checked').value
              })
         };
         let p = fetch('http://localhost:8000/createAccount', requestOptions)
@@ -32,8 +34,17 @@ function NewBankAccount(){
         <>
         <h3>Bank Account:</h3>
         Name <input type="text" id="na"></input> <br/>
-        Country <input type="text" id="country"></input> <br/>
-        AccountType <input type="text" id="acctype"></input> <br/>
+        Country <select  id="country">
+            <option value="England">England</option>
+            <option value="Ireland">Ireland</option>
+            <option value="Scotland">Scotland</option>
+            <option value="Wales">Wales</option>
+        </select><br/>
+        {/* AccountType <input type="text" id="acctype"></input> <br/> */}
+        <input type="radio" id="Current" name="acctype" value="Current"/>
+            <label for="acctype">Current</label><br></br>
+        <input type="radio" id="Savings" name="acctype" value="Savings"/>
+            <label for="acctype">Savings</label><br></br>
         Account Number:<b>{account.accountNumber}</b>
         <br></br>
         <input type="button" value="New Account" onClick={ ()=>createAccount() }></input>
