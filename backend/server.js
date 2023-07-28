606e208c8ae5061a9812779d477b9f99b72306bd
Sad-0ld-GIT <136621076+Sad-0ld-GIT@users.noreply.github.com>
2023-07-28 15:04:30 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -43,4 +43,24 @@ server.post("/saveEmployee",(req,res)=>{
     })
 })
 
+server.post("/createAccount",(req,res)=>{
+    let name= req.body.name
+    let country = req.body.country.substring(0,1)
+    let accountType = req.body.accountType.substring(0,1)
+
+    let findAccountNumberSQL=`select concat('${accountType}','${country}', ifnull( lpad(max(substr(accno,3,3)) + 1,3,'0'),'001')) as newAccno from bank where substr(accno,2,1)='${country}'`;
+    db.query(findAccountNumberSQL,function(err,result){
+        if(err){
+            console.log(err)
+        }else{
+            let insertquery=`insert into bank values('${result[0].newAccno}','${name}')`
+            db.query(insertquery,function(err,result){
+                if (err){
+                    console.log(err)
+                }
+            })
+        }
+    })
+})
+
 server.listen(8000)
